7770578f18ddb6386ab5f133d4479426a16a9161
Seth Knights <knightss@catlin.edu>
2020-06-11 16:01:18 -0700

    Login page more fixed!

--- static/login.js
+++ static/login.js
@@ -2,10 +2,8 @@ function sanitize(text) {
     return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
 }
 
-socket.on("connect", function () {
-    $("form").on("submit", function (e) {
-        e.preventDefault();
-        let room_id = $("input.message").val();
-        window.location = window.location + "chat/" + room_id
-    });
+$("form").on("submit", function (e) {
+    e.preventDefault();
+    let room_id = $("input.message").val();
+    window.location = window.location + "chat/" + room_id
 });
