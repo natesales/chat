f1c0b4de3d9a8b6e39f941b53f00dbd18a967425
Nate Sales <nate@nate.to>
2020-06-11 22:52:08 -0700

    Cleanup peerjs and reformat

--- RealPeerCode/script.js
+++
@@ -1,5 +0,0 @@
-let peer = new Peer( "tpmslmxdae000000", "127.0.0.1", 9000)
-
-peer.on('open', function(id) {
-    console.log('My peer ID is: ' + id);
-  });
--- peerjs/src/transport.js
+++
@@ -1,4 +0,0 @@
-/*
- * transport.js
- * This file contains functions and callbacks for communicating with peers over peerjs sockets.
- */
